1ace01225a1c5805dda4592cb3292ea115d90a65
pgeovanny <geovanny9@gmail.com>
2025-07-13 15:31:17 -0300

    Create schemas.py

---
+++ backend/app/schemas.py
@@ -0,0 +1,26 @@
+from typing import List, Dict, Optional
+from pydantic import BaseModel
+
+class Section(BaseModel):
+    title: str
+    content: str
+
+class SchematizedSection(BaseModel):
+    title: str
+    schematization: str
+
+class ProcessResponse(BaseModel):
+    structure: List[Section]
+    schematization: List[SchematizedSection]
+
+class StructureRequest(BaseModel):
+    structure: List[Section]
+
+class EditRequest(BaseModel):
+    structure: List[Section]
+    schematization: List[SchematizedSection]
+
+class ExportRequest(BaseModel):
+    structure: List[Section]
+    schematization: List[SchematizedSection]
+    export_format: Optional[str] = "pdf"
